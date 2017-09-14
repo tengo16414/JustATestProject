c4f1d695bd8da9affd450b12c105e2e790e0953d
kasradzenika <kasradzenika@yahoo.com>
2017-09-14 17:19:12 +0400

    now i am using frustumHeight to resize background Video Targets size. using Ratio of frustumHeight/bgDistance to calculate Multiplier. using multiplier to resize ImageTargets too. everything seems fine on PC but targets start floating on Android build

--- Assets/Scripts/VuforiaBackgroundSIzePrinter.cs
+++ Assets/Scripts/VuforiaBackgroundSIzePrinter.cs
@@ -6,21 +6,31 @@ public class VuforiaBackgroundSizePrinter : MonoBehaviour
 {
 
     public Vuforia.BackgroundPlaneBehaviour background;
+    public Transform GrandMarker;
+
     public UnityEngine.UI.Text targetText;
 
     public float Multiplier;
     private Vector3 tempScaleVec;
+    //private bool activator = false;
+    private void Start()
+    {
+        InvokeRepeating("ChangeBackgroundScaleUsingFrustumHeight", 2f, 0.5f);
+    }
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
             //ChangeDistance();
-            ChangeBackgroundScale();
+            //ChangeBackgroundScale();
+          //  activator = !activator;
         }
-        
+        //if (activator)
+            //ChangeBackgroundScaleUsingFrustumHeight();
+
 
         string str = string.Format(
-            "{0}\nX: {1} Y: {2} Z: {3}\n{4}\n{5}\n{6}",
+            "{0}\nX: {1} Y: {2} Z: {3}\n{4}\n{5}\n{6}\n{7}",
             //background.NumDivisions.ToString(),
             Multiplier.ToString(),
             background.transform.localScale.x.ToString(),
@@ -28,8 +38,13 @@ public class VuforiaBackgroundSizePrinter : MonoBehaviour
             background.transform.localScale.z.ToString(),
             background.transform.localPosition.ToString(),
             background.transform.parent.localPosition.ToString(),
-            background.transform.parent.parent.localPosition.ToString()
+            background.transform.parent.parent.localPosition.ToString(),
+            (2.0f * background.transform.position.z * Mathf.Tan(Camera.main.fieldOfView * 0.5f * Mathf.Deg2Rad)).ToString()
             );
+        if (background.transform.parent.parent.parent != null)
+        {
+            str = background.transform.parent.parent.parent.gameObject.name;
+        }
         targetText.text = str;
     }
     public void ChangeBackgroundScale()
@@ -37,7 +52,7 @@ public class VuforiaBackgroundSizePrinter : MonoBehaviour
         tempScaleVec = background.transform.localScale;
         if (Multiplier == 1f)
         {
-            tempScaleVec = new Vector3(Screen.width / 2, 1, Screen.height  / 2);
+            tempScaleVec = new Vector3(Screen.width / 2, 1, Screen.height / 2);
             //background.transform.localPosition = new Vector3(0f,0f,1900f);
         }
 
@@ -46,6 +61,31 @@ public class VuforiaBackgroundSizePrinter : MonoBehaviour
 
         background.transform.localScale = tempScaleVec;
         Multiplier += 0.05f;
+    }
+
+    //var frustumHeight = 2.0f * distance * Mathf.Tan(camera.fieldOfView * 0.5f * Mathf.Deg2Rad);
+    public void ChangeBackgroundScaleUsingFrustumHeight()
+    {
+        float frustumHeight = 2.0f * background.transform.localPosition.z * Mathf.Tan(Camera.main.fieldOfView * 0.5f * Mathf.Deg2Rad);
+        Multiplier = frustumHeight / background.transform.localScale.z;
+
+        tempScaleVec = background.transform.localScale;
+
+        Multiplier /= 2f;
+
+        tempScaleVec.x *= Multiplier;
+        tempScaleVec.z *= Multiplier;
+
+        AdjustMarkersSize();
+
+        background.transform.localScale = tempScaleVec;
+    }
+    void AdjustMarkersSize()
+    {
+        foreach (Transform item in GrandMarker)
+        {
+            item.localScale *= Multiplier;
+        }
     }
     public void ChangeDistance()
     {
